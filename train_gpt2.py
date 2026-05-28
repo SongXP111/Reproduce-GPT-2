c37cde655c00b458374b0d2fe27d39513edff12e
xipeng <sxp0728@126.com>
2026-05-27 21:02:31 -0700

    feat: implement MLP, Block, and GPT transformer architecture layers

--- train_gpt2.py
+++ train_gpt2.py
@@ -4,6 +4,32 @@ import torch.nn as nn
 from torch.nn import functional as F
 
 
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embd, 4 * config.n_embd)
+        self.gelu = nn.GELU(approximate='tanh')
+        self.c_proj = nn.Linear(4 * config.n_embd, config.n_embd)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = self.gelu(x)
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = nn.LayerNorm(config.n_embd)
+        self.attn = CausalSelfAttention(config)
+        self.ln_2 = nn.LayerNorm(config.n_embd)
+        self.mlp = MPL(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
 @dataclass
 class GPTConfig:
     block_size: int = 256
@@ -17,3 +43,11 @@ class GPT(nn.Module):
     def __init__(self, config: GPTConfig):
         super().__init__()
         self.config = config
+
+        self.transformer = nn.ModuleDict(
+            wte = nn.Embedding(config.vocab_size, config.n_embd),
+            wpe = nn.Embedding(config.block_size, config.n_embd),
+            h = nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            ln_f = nn.LayerNorm(config.n_embd),
+        )
+        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
