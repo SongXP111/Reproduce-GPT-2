0200d56efcb3598dbc98550b807b878fc1a7db26
xipeng <sxp0728@126.com>
2026-06-11 20:45:29 -0700

    feat: enable bfloat16 autocast for validation and training forward passes

--- train_gpt2.py
+++ train_gpt2.py
@@ -353,7 +353,8 @@ for step in range(max_steps):
             for _ in range(val_loss_steps):
                 x, y = val_loader.next_batch()
                 x, y = x.to(device), y.to(device)
-                logits, loss = model(x, y)
+                with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+                    logits, loss = model(x, y)
                 loss = loss / val_loss_steps
                 val_loss_accum += loss.detach()
         if ddp:
@@ -396,9 +397,8 @@ for step in range(max_steps):
     for micro_step in range(grad_accum_steps):
         x, y = train_loader.next_batch()
         x, y = x.to(device), y.to(device)
-        # with torch.autocast(device_type=device, dtype=torch.bfloat16):
-        #     logits, loss = model(x, y)
-        logits, loss = model(x, y)
+        with torch.autocast(device_type=device_type, dtype=torch.bfloat16):
+            logits, loss = model(x, y)
         loss = loss / grad_accum_steps # normalize the loss
         loss_accum += loss.detach()
         if ddp:
